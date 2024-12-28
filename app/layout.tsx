edd8bce5375e6de286597dc51a3c801f0187603e
airxtech <anshul@airx.me>
2024-12-28 17:37:46 +0530

    Wallet Page Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,40 +17,40 @@ export default function RootLayout({
         <link rel="preload" as="script" href="https://telegram.org/js/telegram-web-app.js" />
         <script src="https://telegram.org/js/telegram-web-app.js" async={false} />
       </head>
-      <TonConnectUIProvider 
-        manifestUrl="https://telegramtest-eight.vercel.app/tonconnect-manifest.json"
-        uiPreferences={{ theme: THEME.DARK }}
-        walletsListConfiguration={{
-          includeWallets: [
-            {
-              appName: "telegram-wallet",
-              name: "Wallet",
-              imageUrl: "https://wallet.tg/images/logo-288.png",
-              aboutUrl: "https://wallet.tg/",
-              universalLink: "https://t.me/wallet?attach=wallet",
-              bridgeUrl: "https://bridge.ton.space/bridge",
-              platforms: ["ios", "android", "macos", "windows", "linux"]
-            },
-            {
-              appName: "tonkeeper",
-              name: "Tonkeeper",
-              imageUrl: "https://tonkeeper.com/assets/tonkeeper-wallet-logo.png",
-              aboutUrl: "https://tonkeeper.com",
-              universalLink: "https://app.tonkeeper.com/ton-connect",
-              bridgeUrl: "https://bridge.tonapi.io/bridge",
-              platforms: ["ios", "android", "chrome"]
-            },
-            // Add other wallets from the example here
-          ]
-        }}
-        actionsConfiguration={{
-          twaReturnUrl: 'https://t.me/v2_zoa_bot_/start'  // Replace with your bot's username
-        }}
-      >
       <body>
-        <AppLayout>{children}</AppLayout>
+        <TonConnectUIProvider 
+          manifestUrl="https://telegramtest-eight.vercel.app/tonconnect-manifest.json"
+          uiPreferences={{ theme: THEME.DARK }}
+          walletsListConfiguration={{
+            includeWallets: [
+              {
+                appName: "telegram-wallet",
+                name: "Wallet",
+                imageUrl: "https://wallet.tg/images/logo-288.png",
+                aboutUrl: "https://wallet.tg/",
+                universalLink: "https://t.me/wallet?attach=wallet",
+                bridgeUrl: "https://bridge.ton.space/bridge",
+                platforms: ["ios", "android", "macos", "windows", "linux"]
+              },
+              {
+                appName: "tonkeeper",
+                name: "Tonkeeper",
+                imageUrl: "https://tonkeeper.com/assets/tonkeeper-wallet-logo.png",
+                aboutUrl: "https://tonkeeper.com",
+                universalLink: "https://app.tonkeeper.com/ton-connect",
+                bridgeUrl: "https://bridge.tonapi.io/bridge",
+                platforms: ["ios", "android", "chrome"]
+              },
+              // Add other wallets from the example here
+            ]
+          }}
+          actionsConfiguration={{
+            twaReturnUrl: 'https://t.me/v2_zoa_bot_/start'  // Replace with your bot's username
+          }}
+        >
+          <AppLayout>{children}</AppLayout>
+        </TonConnectUIProvider>
       </body>
-      </TonConnectUIProvider>
     </html>
   )
 }
