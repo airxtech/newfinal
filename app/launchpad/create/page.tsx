9db5c23ce24d0edc65a2c15247ff2ac6bfcb8c7f
airxtech <anshul@airx.me>
2025-01-04 00:41:02 +0530

    Transaction Issue Resolution Update

--- app/launchpad/create/page.tsx
+++ app/launchpad/create/page.tsx
@@ -77,7 +77,7 @@ export default function CreateTokenPage() {
           messages: [
             {
               address: process.env.WALLET_ADDRESS || '',
-              amount: "1", // 0.3 TON in nanoTONs
+              amount: "300000000", // 0.3 TON in nanoTONs
             }
           ]
         })
